4b6345151f20272bd8762e2ba55f7123c8434aaa
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2024-09-07 22:50:39 +0200

    Add SwiftExtensions dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["Onboarding"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/VPavelDm/SwiftExtensions", branch: "login")
+        .package(url: "https://github.com/VPavelDm/SwiftExtensions", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
