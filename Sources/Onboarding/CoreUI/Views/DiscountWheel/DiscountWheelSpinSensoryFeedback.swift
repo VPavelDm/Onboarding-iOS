2a30de5a1c69936e8030f85b1009241c0221c6a3
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2024-09-27 10:58:22 +0200

    Try new approach

--- Sources/Onboarding/CoreUI/Views/DiscountWheel/DiscountWheelSpinSensoryFeedback.swift
+++ Sources/Onboarding/CoreUI/Views/DiscountWheel/DiscountWheelSpinSensoryFeedback.swift
@@ -9,32 +9,35 @@ import SwiftUI
 
 private struct DiscountWheelSpinSensoryFeedback: ViewModifier, Animatable {
 
-    var currentAngle: Angle
-    var slicesCount: Int
+    var sliceAngle: Angle
+    @Binding var currentAngle: Angle
 
-    var animatableData: Angle {
-        get {
-            currentAngle
-        }
-        set {
-            currentAngle = newValue
-        }
+    init(currentAngle: Binding<Angle>, slicesCount: Int) {
+        self.sliceAngle = Angle.degrees(360.0 / Double(slicesCount))
+        self._currentAngle = currentAngle
     }
 
-    var opacity: CGFloat {
-        min(abs(animatableData.degrees / -1840), 1)
+    var animatableData: Double {
+        get { currentAngle.degrees }
+        set {
+            currentAngle = Angle.degrees(newValue)
+            print(Int(newValue / sliceAngle.degrees))
+        }
     }
 
     func body(content: Content) -> some View {
         content
-            .opacity(opacity)
-//            .sensoryFeedback(feedbackType: .alignment, trigger: animatableData)
+            .sensoryFeedback(feedbackType: .alignment, trigger: Int(animatableData / sliceAngle.degrees))
     }
 }
 
 extension View {
 
-    func wheelSpinSensoryFeedback(currentAngle: Angle, slicesCount: Int) -> some View {
+    func wheelSpinSensoryFeedback(currentAngle: Binding<Angle>, slicesCount: Int) -> some View {
         modifier(DiscountWheelSpinSensoryFeedback(currentAngle: currentAngle, slicesCount: slicesCount))
     }
+}
+
+#Preview {
+    DiscountWheelStepView(step: .testData())
 }
