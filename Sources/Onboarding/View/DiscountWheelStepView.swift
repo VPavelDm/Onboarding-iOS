e5b72f82adcab85e3617be74ffa9682bf55a5539
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2024-11-02 12:00:13 +0100

    Make wheel draggable

--- Sources/Onboarding/View/DiscountWheelStepView.swift
+++ Sources/Onboarding/View/DiscountWheelStepView.swift
@@ -28,6 +28,8 @@ struct DiscountWheelStepView: View {
             titleView
             Spacer()
             DiscountWheel(currentAngle: $currentAngle, slices: slices)
+                .gesture(dragGesture)
+                .readFrame(frame: $wheelFrame, coordinateSpace: .named(String.discountWheelNamespace))
             Spacer()
             Spacer()
             spinButton
@@ -35,6 +37,7 @@ struct DiscountWheelStepView: View {
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(colorPalette.backgroundColor)
+        .coordinateSpace(name: .discountWheelNamespace)
         .sensoryFeedback(feedbackType: .success, trigger: throwConfetti)
         .wheelSpinSensoryFeedback(
             currentAngle: currentAngle,
@@ -54,6 +57,39 @@ struct DiscountWheelStepView: View {
             .multilineTextAlignment(.center)
             .foregroundStyle(colorPalette.primaryTextColor)
     }
+
+    // MARK: - Spinning
+
+    @State private var wheelFrame: CGRect = .zero
+    @State private var previousDraggingAngle: Angle = .zero
+
+    var wheelCenterPoint: CGPoint {
+        CGPoint(
+            x: wheelFrame.origin.x + wheelFrame.width / 2,
+            y: wheelFrame.origin.y + wheelFrame.height / 2
+        )
+    }
+
+    private var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                let currentDraggingPoint = value.location.convertCoordinates(to: wheelFrame.origin)
+                let currentDraggingAngle = wheelCenterPoint.angle(to: currentDraggingPoint)
+                guard previousDraggingAngle != .zero else {
+                    return previousDraggingAngle = currentDraggingAngle
+                }
+                currentAngle.applyDelta(currentDraggingAngle - previousDraggingAngle)
+                previousDraggingAngle = currentDraggingAngle
+            }
+            .onEnded { _ in
+                previousDraggingAngle = .zero
+                withAnimation(.timingCurve(0.2, 0.8, 0.05, 1.0, duration: 10)) {
+                    currentAngle.onDragRelease()
+                }
+            }
+    }
+
+    // MARK: -
 
     private var spinButton: some View {
         AsyncButton {
@@ -97,6 +133,45 @@ private extension Angle {
     static var initialAngle: Angle {
         let slices: [DiscountWheel.Slice] = .slices(colorPalette: .testData)
         return .degrees(360 / Double(slices.count)) / 2
+    }
+
+    mutating func applyDelta(_ delta: Angle) {
+        if delta.degrees > 180 {
+            self += delta - .degrees(360)
+        } else if delta.degrees < -180 {
+            self += delta + .degrees(360)
+        } else {
+            self += delta
+        }
+    }
+
+    mutating func onDragRelease() {
+        let isClockwise = self - Self.initialAngle > .degrees(0)
+        self = .degrees(isClockwise ? -1805 : 1760)
+    }
+}
+
+private extension Hashable where Self == String {
+
+    static var discountWheelNamespace: Self {
+        "DiscountWheel.Namespace"
+    }
+}
+
+private extension CGPoint {
+
+    func convertCoordinates(to point: CGPoint) -> CGPoint {
+        CGPoint(
+            x: point.x + x,
+            y: point.y + y
+        )
+    }
+
+    func angle(to point: CGPoint) -> Angle {
+        let dx = point.x - x
+        let dy = point.y - y
+        let angle = atan2(dy, dx)
+        return .radians(angle)
     }
 }
 
