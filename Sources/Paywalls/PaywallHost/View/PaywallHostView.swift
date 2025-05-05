1e2cbe6bf9f30ecaafd97b742a06415f3a177329
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2025-05-05 14:00:05 +0200

    Create PaywallHostView

--- Sources/Paywalls/PaywallHost/View/PaywallHostView.swift
+++ Sources/Paywalls/PaywallHost/View/PaywallHostView.swift
@@ -2,89 +2,53 @@
 //  File.swift
 //  onboarding-ios
 //
-//  Created by Pavel Vaitsikhouski on 04.05.25.
+//  Created by Pavel Vaitsikhouski on 05.05.25.
 //
 
 import SwiftUI
 import Adapty
 import AdaptyUI
 
-private struct PaywallHostViewModifier: ViewModifier {
+public struct PaywallHostView: View {
 
     private let viewModel: PaywallHostViewModel
-
-    @State private var showRemoteConfigPaywall: AdaptyRemoteConfigPaywall?
-    @State private var paywallConfiguration: AdaptyUI.PaywallConfiguration?
 
     @State private var alertItem: AdaptyAlertItem?
 
     @State private var showRating: Bool = false
 
-    @Binding var isPresented: Bool
-    private let close: () async -> Void
+    @State private var showRemoteConfigPaywall: AdaptyRemoteConfigPaywall?
+    @State private var paywallConfiguration: AdaptyUI.PaywallConfiguration?
 
-    init(isPresented: Binding<Bool>, placementID: String, delegate: any PaywallHostDelegate, close: @escaping () async -> Void) {
-        self._isPresented = isPresented
+    private let close: () -> Void
+
+    public init(placementID: String, delegate: any PaywallHostDelegate, close: @escaping () -> Void) {
         self.viewModel = PaywallHostViewModel(placementID: placementID, delegate: delegate)
         self.close = close
     }
 
-    func body(content: Content) -> some View {
-        VStack {
-            content
-                .background {
-                    if let remoteConfig = showRemoteConfigPaywall {
-                        Color.clear
-                            .fullScreenCover(isPresented: $isPresented) {
-                                Text("")
-//                                PaywallView(
-//                                    products: remoteConfig.products,
-//                                    didPerformAction: didPerformAction(action:),
-//                                    didStartPurchase: didStartPurchase(product:),
-//                                    didFinishPurchase: didFinishPurchase(product:result:),
-//                                    didFailPurchase: didFailPurchase(product:error:),
-//                                    didStartRestore: didStartRestore,
-//                                    didFinishRestore: didFinishRestore(profile:),
-//                                    didFailRestore: didFailRestore(error:),
-//                                    didFail: didFail(error:),
-//                                    showAlertItem: $alertItem,
-//                                    showAlertBuilder: alert(item:)
-//                                )
-//                                .wrapNavigationView()
-                            }
-                    }
-                }
-                .background {
-                    if let paywallConfiguration {
-                        Color.clear
-                            .paywall(
-                                isPresented: $isPresented,
-                                paywallConfiguration: paywallConfiguration,
-                                didPerformAction: didPerformAction(action:),
-                                didStartPurchase: didStartPurchase(product:),
-                                didFinishPurchase: didFinishPurchase(product:result:),
-                                didFailPurchase: didFailPurchase(product:error:),
-                                didStartRestore: didStartRestore,
-                                didFinishRestore: didFinishRestore(profile:),
-                                didFailRestore: didFailRestore(error:),
-                                didFailRendering: didFailRendering(error:),
-                                didFailLoadingProducts: didFailLoadingProducts(error:),
-                                showAlertItem: $alertItem,
-                                showAlertBuilder: alert(item:)
-                            )
-                    }
-                }
-                .ratingSheet(isPresented: $showRating)
-                .onChange(of: isPresented) { [wasPresented = isPresented] nowPresented in
-                    if wasPresented && !nowPresented {
-                        Task { @MainActor in
-                            await close()
-                        }
-                    } else if !wasPresented && nowPresented {
-                        viewModel.delegate?.track(event: "trackPaywallShown", parameters: ["placementID": viewModel.placementID])
-                    }
-                }
+    public var body: some View {
+        Group {
+            if let paywallConfiguration {
+                AdaptyPaywallView(
+                    paywallConfiguration: paywallConfiguration,
+                    didAppear: didAppear,
+                    didDisappear: didDisappear,
+                    didPerformAction: didPerformAction(action:),
+                    didStartPurchase: didStartPurchase(product:),
+                    didFinishPurchase: didFinishPurchase(product:result:),
+                    didFailPurchase: didFailPurchase(product:error:),
+                    didStartRestore: didStartRestore,
+                    didFinishRestore: didFinishRestore(profile:),
+                    didFailRestore: didFailRestore(error:),
+                    didFailRendering: didFailRendering(error:),
+                    didFailLoadingProducts: didFailLoadingProducts(error:),
+                    showAlertItem: $alertItem,
+                    showAlertBuilder: alert(item:)
+                )
+            }
         }
+        .ratingSheet(isPresented: $showRating)
         .task {
             do {
                 let configuration = try await viewModel.fetchPaywallConfiguration()
@@ -102,19 +66,19 @@ private struct PaywallHostViewModifier: ViewModifier {
         }
     }
 
-    func alert(item: AdaptyAlertItem) -> Alert {
-        Alert(
-            title: Text(item.title),
-            message: Text(item.message),
-            dismissButton: .cancel()
-        )
+    func didAppear() {
+
+    }
+
+    func didDisappear() {
+
     }
 
     func didPerformAction(action: AdaptyUI.Action) {
         switch action {
         case .close:
             viewModel.delegate?.track(event: "paywallCloseButtonTapped", parameters: ["placementID": viewModel.placementID])
-            isPresented = false
+            close()
         case .custom(let id):
             switch id {
             case "show_all_plans":
@@ -148,11 +112,11 @@ private struct PaywallHostViewModifier: ViewModifier {
                     "productID": product.sk2Product?.id ?? ""
                 ])
                 showRating = true
-                isPresented = false
+                close()
             case .pending:
                 try await viewModel.delegate?.fetchSubscriptionStatus()
                 viewModel.delegate?.track(event: "subscriptionPending", parameters: ["placementID": viewModel.placementID])
-                isPresented = false
+                close()
             case .userCancelled:
                 viewModel.delegate?.track(event: "subscriptionPurchaseCancelled", parameters: ["placementID": viewModel.placementID])
             }
@@ -179,7 +143,7 @@ private struct PaywallHostViewModifier: ViewModifier {
                 } else {
                     throw StoreError.restorePurchaseError
                 }
-                isPresented = false
+                close()
             } catch {
                 viewModel.delegate?.track(event: "restorePurchaseFailed", parameters: [
                     "placementID": viewModel.placementID,
@@ -219,17 +183,12 @@ private struct PaywallHostViewModifier: ViewModifier {
             "error": error
         ])
     }
-}
 
-public extension View {
-
-    func paywall(
-        isPresented: Binding<Bool>,
-        placementID: String,
-        delegate: any PaywallHostDelegate,
-        close: @escaping () async -> Void = {}
-    ) -> some View {
-        modifier(PaywallHostViewModifier(isPresented: isPresented, placementID: placementID, delegate: delegate, close: close))
+    func alert(item: AdaptyAlertItem) -> Alert {
+        Alert(
+            title: Text(item.title),
+            message: Text(item.message),
+            dismissButton: .cancel()
+        )
     }
 }
-
