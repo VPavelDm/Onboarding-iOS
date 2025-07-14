55ce76220c0a1cebdaba325a844378314b18deef
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2025-07-14 23:11:10 +0200

    add bool popup

--- Sources/CoreUI/ViewModifieris/PopupModifier.swift
+++ Sources/CoreUI/ViewModifieris/PopupModifier.swift
@@ -26,6 +26,37 @@ private struct PopupModifier<Item, PopupContent>: ViewModifier where Item: Ident
 }
 
 @available(iOS 17.0, *)
+private struct PopupBoolModifier<PopupContent>: ViewModifier where PopupContent: View {
+    struct ItemIdentifiable: Identifiable, Sendable, Hashable {
+        var id: UUID = UUID()
+    }
+
+    @State private var item: ItemIdentifiable?
+
+    @Binding var isPresented: Bool
+    @ViewBuilder var popupContent: () -> PopupContent
+    
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: isPresented) { _, newValue in
+                if newValue {
+                    item = ItemIdentifiable()
+                } else {
+                    item = nil
+                }
+            }
+            .shading(item: $item) { _ in
+                popupContent()
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color(uiColor: .systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .padding(32)
+            }
+    }
+}
+
+@available(iOS 17.0, *)
 public extension View {
 
     func popup<Item, PopupContent>(
@@ -33,5 +64,12 @@ public extension View {
         @ViewBuilder content: @escaping (Item) -> PopupContent
     ) -> some View where Item: Identifiable & Hashable, PopupContent: View {
         modifier(PopupModifier(item: item, popupContent: content))
+    }
+
+    func popup<PopupContent>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> PopupContent
+    ) -> some View where PopupContent: View {
+        modifier(PopupBoolModifier(isPresented: isPresented, popupContent: content))
     }
 }
