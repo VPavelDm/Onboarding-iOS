bd4554ea82df300b84a84c514a7db2f1941f9cd6
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2025-07-20 09:11:20 +0200

    revert changes

--- Sources/CoreUI/ViewModifieris/ShadingModifier.swift
+++ Sources/CoreUI/ViewModifieris/ShadingModifier.swift
@@ -49,7 +49,7 @@ private struct ShadingModifier<Item, ShadingContent>: ViewModifier where Item: I
                         isContentVisible = true
                     }
                 }
-                .presentationBackground(self.item != nil ? .clear : Color(.systemBackground))
+                .presentationBackground(.clear)
             }
             .transaction { transaction in
                 transaction.disablesAnimations = true
